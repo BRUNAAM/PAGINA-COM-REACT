a4e2d60736adbd7fa9a59efcb8fe399ac8f38481
BRUNA AMARO <140859773+BRUNAAM@users.noreply.github.com>
2024-08-23 20:45:24 -0300

    atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,85 @@
 import React from "react";
-import Cabecalho from "./componentes/Cabecalho";
-import Menu from "./componentes/Menu";
-import Secao from "./componentes/Secao";
-import Cartao from "./componentes/Cartao";
-import Titulo from "./componentes/Titulo";
-import Formulario from "./componentes/Formulario";
+import Cabecalho from "./components/Cabecalho";
+import Menu from "./components/Menu";
+import Secao from "./components/Secao";
+import Cartao from "./components/Cartao";
+import Titulo from "./components/Titulo";
+import Formulario from "./components/Formulario";
+import "./App.css";
+
+import { collection, getDocs } from "firebase/firestore";
+import db from "./database/firebaseConfig";
 
 const App = () => {
+  //Busca todos os documentos da coleção "comentarios"
+  //e lista no console cada um dos documentos salvos
+  const lerBanco = async () => {
+    const comentarios = await getDocs(collection(db, "comentarios"));
+    comentarios.forEach((documento) => {
+      console.log(documento.data());
+    });
+  };
+
+  const campos = [
+    //VETOR de OBJETOS
+    {
+      //Começa o Objeto
+      nome: "Nome completo",
+      id: "nome",
+      tipo: "text",
+    }, //Fim do OBJETO
+    {
+      nome: "Email Válido",
+      id: "email",
+      tipo: "email",
+    },
+    {
+      nome: "Cidade",
+      id: "cidade",
+      tipo: "text",
+    },
+    {
+      nome: "Estado",
+      id: "estado",
+      tipo: "text",
+    },
+    {
+      nome: "Telefone de Contato",
+      id: "fone",
+      tipo: "text",
+    },
+    {
+      nome: "Deixe seu recado",
+      id: "recado",
+      tipo: "text",
+    },
+  ];
+
   return (
     <div>
       <Cabecalho />
       <Menu />
       <Secao>
-        <img src="src/assets/imagem.webp" alt="Imagem Descritiva" />
+        <img src="src/assets/topo.jpg"></img>
       </Secao>
+
       <Secao>
         <Titulo texto="Serviços" />
-        <Cartao />
+        <Cartao
+          descricao="Um texto descritivo"
+          imagem="#"
+          titulo="Um LOGO BEM GRANDE"
+        />
+        <Cartao descricao="LOREN IPSUN" imagem="#" titulo="Um TITULO" />
       </Secao>
+
       <Secao>
         <Titulo texto="Contato" />
-        <Formulario campos={[{ id: 'nome', nome: 'Nome', tipo: 'text' }, { id: 'email', nome: 'Email', tipo: 'email' }]} />
+
+        <Formulario campos={campos} />
+        <button onClick={lerBanco}>Ler dados do Banco</button>
       </Secao>
-
-
     </div>
   );
 };
-
 export default App;
